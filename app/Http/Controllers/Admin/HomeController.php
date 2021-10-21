63af410bde42eeef5f7c32afb91c2cffa43593ae
Neto Guimarães <neto--guimaraes1@hotmail.com>
2021-10-21 16:39:52 -0300

    Dashboard: Estrutura, Informações, Gráfico, Dados.

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 Use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Visitor;
+use App\Page;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -12,7 +15,51 @@ class HomeController extends Controller
        $this->middleware('auth');
     }
     
-    public function index () {
-        return view('admin.home');
+    public function index (Request $request) {
+
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+        $interval = intval($request->input('interval', 30));
+        if($interval >120) {
+            $interval = 120;
+        }
+
+        //Contagem de visitantes
+        $dateInterval = date('Y-m-d H-i-s', strtotime('-'.$interval.' days'));
+        $visitsCount = Visitor::where('date_acess', '>=', $dateInterval)->count();
+
+        //Contagem de usuários online
+        $datelimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')->where('date_acess', '>=', $datelimit)->groupBy('ip')->get();
+        $onlineCount = count($onlineList);
+
+
+
+        // Contagem de páginas 
+        $pageCount = Page::count();
+
+        //Contagem de Usuários 
+        $userCount = User::count();
+
+    // Contagem para o pagepie
+          $visitsAll = Visitor::selectRaw('page, count(page) as c')->where('date_acess', '>=', $dateInterval)->groupBy('page')->get();
+          foreach($visitsAll as $visit) {
+              $pagePie[ $visit['page'] ] = intval($visit['c']);
+          }
+
+        $pageLabels = json_encode( array_keys($pagePie) );
+        $pageValues = json_encode( array_values($pagePie) );
+
+        return view('admin.home', [
+            'visitsCount' => $visitsCount,
+            'onlineCount' => $onlineCount,
+            'pageCount' => $pageCount,
+            'userCount' => $userCount,
+            'pageLabels' => $pageLabels,
+            'pageValues' => $pageValues,
+            'dateInterval' => $interval
+        ]);
     }
 }
